037bf632ed228ed7570a94ec89d006766d622633
David Heidrich <me@bowlingx.com>
2017-01-22 00:54:44 +0100

    use gpg

--- scripts/download_pgp_key.sh
+++ scripts/download_pgp_key.sh
@@ -5,8 +5,8 @@ if [[ $KEYSTORE_DIR && ${KEYSTORE_DIR} && $KEYSTORE_URI_PUB && ${KEYSTORE_URI_PU
 then
     echo "Downloading PGP file..."
     mkdir -p ${KEYSTORE_DIR}
-    curl -L -o ${KEYSTORE_DIR}/pubring.asc ${KEYSTORE_URI_PUB}
-    curl -L -o ${KEYSTORE_DIR}/secring.asc ${KEYSTORE_URI_PRIV}
+    curl -L -o ${KEYSTORE_DIR}/pubring.gpg ${KEYSTORE_URI_PUB}
+    curl -L -o ${KEYSTORE_DIR}/secring.gpg ${KEYSTORE_URI_PRIV}
 else
     echo "Keystore uri(s) not set. No PGP signing available."
 fi
