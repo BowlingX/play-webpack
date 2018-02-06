666719e0f32abb4734df45d93d56339d6cfc7f83
David Heidrich <me@bowlingx.com>
2018-02-06 20:49:59 +0100

    fixed default test

--- project/PlayWebpackBuild.scala
+++ project/PlayWebpackBuild.scala
@@ -68,10 +68,10 @@ object PlayWebpackBuild {
              |package com.bowlingx.webpack
              |
              |object WebpackManifest extends WebpackManifestType {
-             |  val entries:Map[String, WebpackEntry] = Map(
-             |  "vendor" -> WebpackEntry(Some("https://localhost:8080/assets/scripts/vendor.js"), None),
-             |  "polyfills" -> WebpackEntry(Some("/assets/scripts/polyfills.js"), None),
-             |  "server" -> WebpackEntry(Some("/assets/scripts/server.js"), None)
+             |  val entries:Map[String, Either[WebpackEntry, String]] = Map(
+             |  "vendor" -> Left(WebpackEntry(Some("https://localhost:8080/assets/scripts/vendor.js"), None)),
+             |  "polyfills" -> Left(WebpackEntry(Some("/assets/scripts/polyfills.js"), None)),
+             |  "server" -> Left(WebpackEntry(Some("/assets/scripts/server.js"), None))
              |  )
              |}
      """.stripMargin
